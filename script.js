5fd1ad660f6383f2ea2ee3a14a6d10229a4c061e
jet tang <jettang@jets-MacBook-Pro.local>
2017-07-21 11:16:50 +0800

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	document.write("浏览器分辨率是"+document.documentElement.clientWidth
+		+"*"+document.documentElement.clientHeight ); 
+	document.write("屏幕分辨率是"+screen.width+"*"+screen.height);
+	
+}
